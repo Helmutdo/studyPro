5108d84acf815c8662c39257ce1cc23bd5e32dc2
Helmutdo <helmut.schweitzerg@gmail.com>
2024-09-09 13:21:06 -0300

    creacion de 2 ramas nuevas con un pull desde git

--- studyPro/asgi.py
+++ studyPro/asgi.py
@@ -14,3 +14,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studyPro.settings')
 
 application = get_asgi_application()
+
+
+# COMENTARIO RAMA NEUVA NEUVA
